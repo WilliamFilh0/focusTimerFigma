4eb2bebdbb5d0d1c42c744da18dcacdcc31691fe
WilliamFilh0 <williamfilho8908@gmail.com>
2023-08-10 11:22:26 -0300

    Avançando na logica do contador(Minutes e seconds)

--- script.js
+++ script.js
@@ -1,22 +1,35 @@
 const buttonPlay = document.querySelector('.play')
 const buttonPause = document.querySelector('.pause')
-const buttonStop = document.querySelector( '.stop')
+const buttonStop = document.querySelector('.stop')
 const buttonSet = document.querySelector('.set')
 const buttonSoundOn = document.querySelector('.sound-on')
 const buttonSoundOff = document.querySelector('.sound-off')
-let minutes 
+let minutes
 const minutesDisplay = document.querySelector('.minutes')
 const secondsDisplay = document.querySelector('.seconds')
 
-function countdown(){
-  setTimeout(function(){
+function countdown() {
+  setTimeout(function () {
     let seconds = Number(secondsDisplay.textContent)
+    let minutes = Number(minutesDisplay.textContent)
 
-    if(seconds <=0 ){
-      seconds = 60
+    secondsDisplay.textContent = String(seconds - 1).padStart(2, "0")
+
+    if (minutes <= 0) {
+      buttonPlay.classList.remove('hide')
+      buttonPause.classList.add('hide')
+      buttonSet.classList.remove('hide')
+      buttonStop.classList.add('hide')
+      return
     }
 
-    secondsDisplay.textContent = seconds -1
+
+    if (seconds <= 0) {
+      seconds = 60
+
+      minutesDisplay.textContent = String(minutes - 1).padStart(2, "0")
+    }
+
     countdown()
   }, 1000)
 }
@@ -30,29 +43,29 @@ buttonPlay.addEventListener('click', function () {
   countdown()
 })
 
-buttonPause.addEventListener('click', function(){
+buttonPause.addEventListener('click', function () {
   buttonPause.classList.add('hide')
   buttonPlay.classList.remove('hide')
 })
 
-buttonStop.addEventListener('click', function(){
+buttonStop.addEventListener('click', function () {
   buttonPlay.classList.remove('hide')
   buttonPause.classList.add('hide')
   buttonSet.classList.remove('hide')
   buttonStop.classList.add('hide')
 })
 
-buttonSoundOff.addEventListener('click', function(){
+buttonSoundOff.addEventListener('click', function () {
   buttonSoundOn.classList.remove('hide')
   buttonSoundOff.classList.add('hide')
 })
 
-buttonSoundOn.addEventListener('click', function(){
+buttonSoundOn.addEventListener('click', function () {
   buttonSoundOn.classList.add('hide')
   buttonSoundOff.classList.remove('hide')
 })
 
-buttonSet.addEventListener('click', function(){
+buttonSet.addEventListener('click', function () {
   minutes = prompt('Quantos minutos?')
-  minutesDisplay.textContent = minutes
+  minutesDisplay.textContent = String(minutes).padStart(2, "0")
 })
