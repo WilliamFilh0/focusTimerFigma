4a40abc83f0ab999c359876aa392da61c909c44a
WilliamFilh0 <williamfilho8908@gmail.com>
2023-08-12 12:23:46 -0300

    Resetando o cronometro

--- script.js
+++ script.js
@@ -4,43 +4,53 @@ const buttonStop = document.querySelector('.stop')
 const buttonSet = document.querySelector('.set')
 const buttonSoundOn = document.querySelector('.sound-on')
 const buttonSoundOff = document.querySelector('.sound-off')
-let minutes
 const minutesDisplay = document.querySelector('.minutes')
 const secondsDisplay = document.querySelector('.seconds')
+let minutes = Number(minutesDisplay.textContent)
+let timerTimeOut
 
-function resetControls(){
+//reseta os controles pra os controles de inicio da aplicação 
+function resetControls() {
   buttonPlay.classList.remove('hide')
   buttonPause.classList.add('hide')
   buttonSet.classList.remove('hide')
   buttonStop.classList.add('hide')
 }
 
-function updateTimerDisplay(minutes, seconds){
+//A função updateTimerDisplay() é chamada para atualizar o valor exibido na contagem regressiva
+function updateTimerDisplay(minutes, seconds) {
+  //Completa o numero com um carcter com um "0" na frente 
   minutesDisplay.textContent = String(minutes).padStart(2, "0")
   secondsDisplay.textContent = String(seconds).padStart(2, "0")
 }
 
+function resetTimer() {
+  updateTimerDisplay(minutes, 0)
+  clearTimeout(timerTimeOut)
+}
+
+
+//A função countdown() utiliza a função setTimeout() para agendar a execução de um bloco de código após um atraso de 1000 milissegundos (1 segundo). Isso permite que a contagem regressiva ocorra a cada segundo.
 function countdown() {
-  setTimeout(function () {
+  timerTimeOut = setTimeout(function () {
+    //Para obter o valor atual dos minutos a partir do elemento de exibição correspondente.
     let seconds = Number(secondsDisplay.textContent)
     let minutes = Number(minutesDisplay.textContent)
 
     updateTimerDisplay(minutes, 0)
 
-     if (minutes <= 0) {
-      
+    if (minutes <= 0) {
       resetControls()
       return
     }
 
 
     if (seconds <= 0) {
-      seconds = 60
+      seconds = 3
       --minutes
-
     }
 
-    updateTimerDisplay(minutes,  String(seconds - 1))
+    updateTimerDisplay(minutes, String(seconds - 1))
 
     countdown()
   }, 1000)
@@ -58,10 +68,13 @@ buttonPlay.addEventListener('click', function () {
 buttonPause.addEventListener('click', function () {
   buttonPause.classList.add('hide')
   buttonPlay.classList.remove('hide')
+  //Parar o cronometro
+  clearTimeout(timerTimeOut)
 })
 
 buttonStop.addEventListener('click', function () {
-    resetControls()
+  resetControls()
+  resetTimer()
 })
 
 buttonSoundOff.addEventListener('click', function () {
@@ -74,7 +87,8 @@ buttonSoundOn.addEventListener('click', function () {
   buttonSoundOff.classList.remove('hide')
 })
 
+//botton pra determinar os minutos 
 buttonSet.addEventListener('click', function () {
-  minutes = prompt('Quantos minutos?')
+  minutes = prompt('Quantos minutos?') || 0
   updateTimerDisplay(minutes, 0)
 })
