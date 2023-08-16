27692c9c202d52241d7ffdf31a4f80a15278fbd0
WilliamFilh0 <williamfilho8908@gmail.com>
2023-08-16 10:06:29 -0300

    Criando arquivo elements

--- js/index.js
+++ js/index.js
@@ -3,15 +3,17 @@ import Controls from "./controls.js"
 //named import
 import Timer from "./timer.js"
 
+import { 
+  buttonPause,
+  buttonPlay,
+  buttonSet,
+  buttonSoundOff,
+  buttonSoundOn,
+  buttonStop, 
+  minutesDisplay,
+  secondsDisplay,
+ } from "./elements.js"
 
-const buttonPlay = document.querySelector('.play')
-const buttonPause = document.querySelector('.pause')
-const buttonStop = document.querySelector('.stop')
-const buttonSet = document.querySelector('.set')
-const buttonSoundOn = document.querySelector('.sound-on')
-const buttonSoundOff = document.querySelector('.sound-off')
-const minutesDisplay = document.querySelector('.minutes')
-const secondsDisplay = document.querySelector('.seconds')
 
 
 const controls = Controls({
